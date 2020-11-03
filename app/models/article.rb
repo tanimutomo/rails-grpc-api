165397cad57e9a351da6fb7b8ce8ba9c0ba7fade
tanimutomo <tanimutomo@gmail.com>
2020-11-03 14:24:50 +0900

    add: schema annotation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
 end
